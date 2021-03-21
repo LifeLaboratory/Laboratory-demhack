110d0c123d82ff2d93d97593a4de28ce90438b8c
Boris Bochkaryov <boris-bochkaryov@yandex.ru>
2021-03-21 09:36:48 +0700

    Смена логотипа

--- Front/src/views/Home.vue
+++ Front/src/views/Home.vue
@@ -2,7 +2,7 @@
   <a-row type="flex" justify="center">
     <a-col :span="35">
       <div class="box">
-        <img alt="Vue logo" src="../assets/logo_demhack.png" width="50%" style="margin-top: 50px;">
+        <img alt="Vue logo" src="../assets/logo_life.png" width="90%" style="margin-top: 50px;">
         <h1 style="margin-top: 50px; margin-bottom: 50px; font-size: 32pt;">Добро пожаловать в "Интернет"!</h1>
         <a-button type="primary" class="start-button" v-on:click="toAuth" style="margin-bottom: 50px; height: 75px; font-size: 20pt;">
           Подключиться
